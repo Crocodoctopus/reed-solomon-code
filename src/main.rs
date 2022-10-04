ea7715a0a34602aa958396e78958653910697621
akroune <crocodoctopus@gmail.com>
2022-10-03 18:19:05 -0800

    minor cleanup

--- src/main.rs
+++ src/main.rs
@@ -23,51 +23,40 @@ fn rs_lagrange(points: Vec<(i32, i32)>, p: u32) -> Polynomial<u32> {
         .map(|x| mod_inv(x, p))
         .collect();
 
-    // Roots
-    let roots: Vec<Polynomial<i32>> = points
-        .iter()
-        .map(|(x, _)| Polynomial::new_degree2(1, -x))
-        .collect();
-
     // Generate non-normalized basis polynomials
     let mut bases: Vec<Polynomial<i32>> = {
         // Generate left polynomial expansion
         let mut ll = vec![Polynomial::new_degree1(1i32); points.len()];
         for i in 1..points.len() {
-            ll[i] = ll[i - 1].clone() * roots[i - 1].clone();
+            ll[i] = ll[i - 1].clone() * Polynomial::new_degree2(1, -points[i - 1].0);
         }
+
         // Generate right polynomial expansion
         let mut lr = vec![Polynomial::new_degree1(1); points.len()];
         for i in (0..points.len() - 1).rev() {
-            lr[i] = lr[i + 1].clone() * roots[i + 1].clone();
+            lr[i] = lr[i + 1].clone() * Polynomial::new_degree2(1, -points[i + 1].0);
         }
+
         // Combine
         std::iter::zip(ll, lr).map(|(l, r)| l * r).collect()
     };
 
-    // Normalize
+    // Apply factors to "normalize" bases
     for (i, poly) in bases.iter_mut().enumerate() {
-        let factor = nf[i];
         let y = points[i].1;
-        for coeff in poly.0.iter_mut() {
-            *coeff *= factor * y;
-        }
+        *poly *= nf[i] * y;
     }
 
-    // Generate final lagrange polynomial
-    let mut f: Vec<u32> = vec![0; points.len()];
-    for i in 0..points.len() {
-        let x: i32 = bases.iter().map(|ln| ln.0[i]).sum();
-        f[i] = mod_norm(x, p) % p;
-    }
-
-    println!("final: {f:?}");
-
-    Polynomial(f)
+    // Add all bases to generate final polynomial in 'mod p'
+    let coeffs = (0..points.len())
+        .map(|i| mod_norm(bases.iter().map(|ln| ln.0[i]).sum(), p) % p)
+        .collect();
+    Polynomial(coeffs)
 }
 
 fn main() {
-    rs_lagrange(vec![(0, 2), (1, 4), (2, 3), (3, 1)], 5);
+    let poly = rs_lagrange(vec![(0, 2), (1, 4), (2, 3), (3, 1)], 5);
+    println!("final: {poly:?}");
 }
 
 /*
